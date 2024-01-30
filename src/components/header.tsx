bb97f6fee66423a90d1577703d4f7f22e86c6ff9
JoaoVLima <celu.joaolima@gmail.com>
2024-01-30 11:24:22 -0300

    css and swipper

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export function Header() {
                     <div className="flex lg:hidden">
                         <button
                             type="button"
-                            className="inline-flex items-center justify-center rounded-md text-gray-700 -m-2.5 p-2.5 dark:text-white"
+                            className="inline-flex items-center justify-center rounded-md text-neutral-700 -m-2.5 p-2.5 dark:text-white"
                             onClick={() => setMobileMenuOpen(true)}
                         >
                             <span className="sr-only">Open main menu</span>
@@ -37,20 +37,20 @@ export function Header() {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-neutral-900 dark:text-white">
                                 {item.name}
                             </a>
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                        <a href="#" className="text-sm font-semibold leading-6 text-neutral-900 dark:text-white">
                             Log in <span aria-hidden="true">&rarr;</span>
                         </a>
                     </div>
                 </nav>
                 <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                     <div className="fixed inset-0 z-50"/>
-                    <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 dark:bg-gray-900 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 dark:bg-neutral-900 sm:max-w-sm sm:ring-1 sm:ring-neutral-900/10">
                         <div className="flex items-center justify-between">
                             <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Lima.dev</span>
@@ -62,7 +62,7 @@ export function Header() {
                             </a>
                             <button
                                 type="button"
-                                className="rounded-md text-gray-700 -m-2.5 p-2.5"
+                                className="rounded-md text-neutral-700 -m-2.5 p-2.5"
                                 onClick={() => setMobileMenuOpen(false)}
                             >
                                 <span className="sr-only">Close menu</span>
@@ -70,13 +70,13 @@ export function Header() {
                             </button>
                         </div>
                         <div className="mt-6 flow-root">
-                            <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="-my-6 divide-y divide-neutral-500/10">
                                 <div className="py-6 space-y-2">
                                     {navigation.map((item) => (
                                         <a
                                             key={item.name}
                                             href={item.href}
-                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-gray-700"
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-neutral-900 hover:bg-neutral-50 dark:text-white dark:hover:bg-neutral-700"
                                         >
                                             {item.name}
                                         </a>
@@ -85,7 +85,7 @@ export function Header() {
                                 <div className="py-6">
                                     <a
                                         href="#"
-                                        className="-mx-3 block rounded-lg px-3 text-base font-semibold leading-7 text-gray-900 py-2.5 hover:bg-gray-50 dark:text-white"
+                                        className="-mx-3 block rounded-lg px-3 text-base font-semibold leading-7 text-neutral-900 py-2.5 hover:bg-neutral-50 dark:text-white"
                                     >
                                         Log in
                                     </a>
