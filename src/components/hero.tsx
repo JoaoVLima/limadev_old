f9bddc8df28ca977ec581976baaa08012c348d3b
JoaoVLima <celu.joaolima@gmail.com>
2024-01-30 19:25:55 -0300

    prototyping swiper

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,29 +13,73 @@ import {Keyboard, Mousewheel, HashNavigation} from 'swiper/modules';
 export function Hero() {
     return (
         <>
-            <div className="pt-24 pb-10 h-svh">
+            <div className="h-svh min-h-96 pt-28 pb-10 px-2">
                 <Swiper
                     className="h-full w-full"
                     slidesPerView={"auto"}
                     centeredSlides={true}
                     spaceBetween={30}
+                    initialSlide={1}
                     hashNavigation={{
                         watchState: true,
+                        replaceState: true,
                     }}
-                    keyboard={{
-                        enabled: true,
+                    keyboard={true}
+                    grabCursor={true}
+                    mousewheel={{
+                        forceToAxis: true,
                     }}
-                    mousewheel={true}
                     navigation={false}
-
                     modules={[Keyboard, Mousewheel, HashNavigation]}
-
                 >
-                    <SwiperSlide className="rounded-r-3xl bg-neutral-900 text-center text-white" data-hash="Brenda">
-                        <h1>Brenda</h1>
+                    <SwiperSlide className="rounded-3xl bg-neutral-900 text-center text-white" data-hash="Brenda">
+                        <Swiper
+                            className="h-full w-full"
+                            direction={'vertical'}
+                            slidesPerView={"auto"}
+                            centeredSlides={true}
+                            spaceBetween={30}
+                            keyboard={true}
+                            grabCursor={true}
+                            mousewheel={{
+                                forceToAxis: true,
+                            }}
+                            navigation={false}
+                            modules={[Keyboard, Mousewheel]}
+                        >
+                            <SwiperSlide className="rounded-3xl text-center text-white">
+                                <h1>Brenda</h1>
+                            </SwiperSlide>
+                            <SwiperSlide className="rounded-3xl text-center text-white">
+                                <h1>Portifolio</h1>
+                            </SwiperSlide>
+                        </Swiper>
                     </SwiperSlide>
-                    <SwiperSlide className="rounded-l-3xl bg-neutral-900 text-center text-white" data-hash="Joao">
-                        <h1>Joao</h1>
+                    <SwiperSlide className="!w-1" data-hash=" ">
+                        {/* Middle */}
+                    </SwiperSlide>
+                    <SwiperSlide className="rounded-3xl bg-neutral-900 text-center text-white" data-hash="Joao">
+                        <Swiper
+                            className="h-full w-full"
+                            direction={'vertical'}
+                            slidesPerView={"auto"}
+                            centeredSlides={true}
+                            spaceBetween={30}
+                            keyboard={true}
+                            grabCursor={true}
+                            mousewheel={{
+                                forceToAxis: true,
+                            }}
+                            navigation={false}
+                            modules={[Keyboard, Mousewheel]}
+                        >
+                            <SwiperSlide className="rounded-3xl text-center text-white">
+                                <h1>Joao</h1>
+                            </SwiperSlide>
+                            <SwiperSlide className="rounded-3xl text-center text-white">
+                                <h1>Portifolio</h1>
+                            </SwiperSlide>
+                        </Swiper>
                     </SwiperSlide>
                 </Swiper>
             </div>
