e3caada56174c83f60543e20f909f488b352ad13
JoaoVLima <celu.joaolima@gmail.com>
2024-02-01 23:03:27 -0300

    Adding hand animation and fadeout

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Keyboard, Mousewheel, HashNavigation} from 'swiper/modules';
 import 'swiper/css';
-import {SwipeHand} from "./swipehand.tsx";
+import {SwipeHand, HideHand} from "./swipehand.tsx";
 
 export function Hero() {
     return (
@@ -27,6 +27,9 @@ export function Hero() {
                     }}
                     navigation={false}
                     modules={[Keyboard, Mousewheel, HashNavigation]}
+                    onClick={HideHand}
+                    onDrag={HideHand}
+                    onTouchStart={HideHand}
                 >
                     <SwiperSlide className="rounded-3xl bg-neutral-900 text-center text-white" data-hash="Brenda">
                         <h1>Brenda</h1>
--- src/components/swipehand.tsx
+++ src/components/swipehand.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './swipehand.css'
 
+document.addEventListener('scroll', HideHand);
+document.addEventListener('keydown', HideHand);
+document.addEventListener('click', HideHand);
+document.addEventListener('touchstart', HideHand);
+
 export function SwipeHand() {
     return (
         <>
             <div id="swipehand" className="absolute top-1/2 left-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
-                <div className="relative h-10 w-10 rounded-full bg-fuchsia-600 opacity-20 invisible path"></div>
-                <img src="./hand.svg" className="relative  h-24 w-24 hand-icon"></img>
+                <div className="relative top-12 right-2 h-7 w-7 rounded-full bg-fuchsia-600 opacity-20 dot-icon"></div>
+                <div className="relative top-5 right-2 h-7 w-7 rounded-full bg-gradient-to-l opacity-80 from-fuchsia-600 path"></div>
+                <img src="./hand.svg" className="relative h-16 w-16 origin-bottom hand-icon"></img>
             </div>
         </>
     )
+}
+
+export function HideHand(){
+    const swipehand = document.getElementById('swipehand') as HTMLDivElement;
+    swipehand.classList.add("hidehand");
 }
